352d9de09211f2f5e42c7d728c4cbe97ced8f36a
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2022-11-17 17:26:34 -0300

    Classe de personagem está estruturado
    Agora podemos avançar

--- MiniChallenge#2/MiniChallenge#2/Controller/GameViewController.swift
+++ MiniChallenge#2/MiniChallenge#2/Controller/GameViewController.swift
@@ -14,6 +14,9 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //test
+        Character.character.characterToCollide(character: AnimatedObject("marcio"))
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
--- MiniChallenge#2/MiniChallenge#2/Model/Character.swift
+++ MiniChallenge#2/MiniChallenge#2/Model/Character.swift
@@ -26,8 +26,9 @@ public class Character {
         //Properties of size and position
         character.size = CGSize(width: 100, height: 100)
         character.position = CGPoint(x: 0 - character.frame.width, y: 0)
+        character.zPosition = 5
         
-        //Phisics properties
+        //Physics properties
         character.physicsBody = SKPhysicsBody(rectangleOf: character.frame.size)
         character.physicsBody?.affectedByGravity = true
         character.physicsBody?.isDynamic = true
@@ -35,10 +36,20 @@ public class Character {
         return character
     }
     
+    //Setting impulse to the object, this impulse makes the objetc fly
     func characterToFly(character: SKSpriteNode) -> SKSpriteNode {
-        let character = SKSpriteNode()
         
+        character.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 250))
         
+        return character
+    }
+    
+    //Setting properties of collision and contact
+    func characterToCollide(character: SKSpriteNode) -> SKSpriteNode {
+        character.physicsBody?.categoryBitMask = PhysicsCategory.character
+        character.physicsBody?.contactTestBitMask = PhysicsCategory.ground | PhysicsCategory.obstacle
+        character.physicsBody?.collisionBitMask = PhysicsCategory.ground | PhysicsCategory.obstacle
         
         return character
     }
