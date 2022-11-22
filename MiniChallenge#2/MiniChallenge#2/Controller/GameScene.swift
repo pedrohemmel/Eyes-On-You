49ea2ab72389de39a77321675c783817420fb645
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2022-11-22 15:30:35 -0300

    Mudando os comentários para português por causa do gustavinha

--- MiniChallenge#2/MiniChallenge#2/Controller/GameScene.swift
+++ MiniChallenge#2/MiniChallenge#2/Controller/GameScene.swift
@@ -15,39 +15,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         
-        //Relating the SKPhysicsContactDelegate to the self class
+        //Relacionando o SKPhysicsContactDelegate à classe self
         self.physicsWorld.contactDelegate = self
         
-        //Creating and calling the functions that make structure of the character
+        //Criando e chamando as funções que fazem a estrutura do personagem
         character.characterView = SKSpriteNode(imageNamed: "examploOfCharacter")
         character.characterView = character.characterToApplyProperties(character: character.characterView, view: self)
         character.characterView = character.characterToCollide(character: character.characterView)
         
-        //Creating and calling the functions that make structure of the obstacle
+        //Criando e chamando as funções que fazem a estrutura do obstáculo
         obstacle.obstacleView = obstacle.obstacleToCollide(obstacle: obstacle.obstacleView)
         obstacle.obstacleView.position = CGPoint(x: self.frame.width / 2 - obstacle.obstacleView.frame.width, y: self.frame.height / 2)
         
-        //Creating the ground object
+        //Criando o objeto solo
         self.ground = groundToCreate(ground: self.ground)
         
-        //Adding the children to the self class
+        //Adicionando os filhos à autoclasse
         self.addChild(character.characterView)
         self.addChild(obstacle.obstacleView)
         self.addChild(ground)
     }
     
-    //Function that detect the contact of the bodies
+    //Função que detecta o contato dos corpos
     func didBegin(_ contact: SKPhysicsContact) {
         let firstBody = contact.bodyA
         let secondBody = contact.bodyB
         
-        //Conditional structure that verify the contact bodies
+        //Estrutura condicional que verifica os corpos de contato
         if firstBody.categoryBitMask == PhysicsCategory.character && secondBody.categoryBitMask == PhysicsCategory.obstacle || firstBody.categoryBitMask == PhysicsCategory.obstacle && secondBody.categoryBitMask == PhysicsCategory.character {
             self.character.characterView = character.characterToFly(character: self.character.characterView)
         }
     }
     
-    //MARK: - Creating objects of the main scene
+    //MARK: - Criando objetos da cena principal
     func groundToCreate(ground: SKSpriteNode) -> SKSpriteNode {
         ground.size = CGSize(width: self.frame.width, height: 100)
         ground.position = CGPoint(x: 0, y: 0)
